2eb1e050eafb5571ea6a8f248a351adcd68a520f
daugardas <luksasdaugardas@gmail.com>
2024-09-10 22:23:51 +0300

    Initial commit

---
+++ src/main/java/com/learntrack/learntrack/LearnTrackApplication.java
@@ -0,0 +1,13 @@
+package com.learntrack.learntrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnTrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learntrack/learntrack/LearnTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learntrack.learntrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
